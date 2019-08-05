59c3b64c4c998a267aa5aaf0beee259f89af0f5d
A-312 <A-312@users.noreply.github.com>
2019-08-05 14:25:44 +0200

    first commit

---
+++ dep-avion/index.js
@@ -0,0 +1,58 @@
+const convict = require('convict');
+
+convict.addFormat({
+  name: 'source-array',
+  validate: function(sources, schema) {
+    if (!Array.isArray(sources)) {
+      throw new Error('must be of type Array');
+    }
+
+    for (source of sources) {
+      convict(schema.children).load(source).validate();
+    }
+  }
+});
+
+const schema = {
+  sources: {
+    doc: 'A collection of data sources.',
+    format: 'source-array',
+    default: [],
+
+    children: {
+      type: {
+        doc: 'The source type',
+        format: ['git', 'hg', 'svn'],
+        default: null
+      },
+      url: {
+        doc: 'The source URL',
+        format: 'url',
+        default: null
+      }
+    }
+  }
+};
+
+const config = convict(schema).load({
+  'sources': [
+    {
+      'type': 'git',
+      'url': 'https://github.com/mozilla/node-convict.git'
+    },
+    {
+      'type': 'git',
+      'url': 'https://github.com/github/hub.git'
+    }
+  ]
+});
+
+(() => {
+  try {
+    config.validate();
+  } catch (e) {
+    return console.log("Ok! Node use old version of conflict ! :D")
+  }
+
+  throw Error("Error conflict");
+})();
